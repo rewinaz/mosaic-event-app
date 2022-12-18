b6c900005f3c45f78fddd7005014b24c518f265f
Rewina <zerourewina9@gmail.com>
2022-12-18 07:00:24 +0300

    web added

--- lib/components/onboarding/onboarding_screen_page.dart
+++ lib/components/onboarding/onboarding_screen_page.dart
@@ -5,20 +5,40 @@ class OnboardingScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Column(
         children: [
-          Image.asset("lib/assets/images/onboarding_1.png"),
-          Column(
-            children: [
-              Text("Welcome to \nMosaic Event App"),
-              Text(
-                  "Find the best event near you with just one of the best app."),
-              MaterialButton(
-                onPressed: () {},
-                child: Text("Get Started"),
-              )
-            ],
+          Container(
+              height: screenHeight * 0.5,
+              child: Image.asset("lib/assets/images/onboarding_1.png")),
+          Container(
+            width: double.maxFinite,
+            height: screenHeight * 0.5,
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+                border: Border.fromBorderSide(BorderSide(color: Colors.red))),
+            child: Column(
+              children: [
+                Text(
+                  "Welcome to \nMosaic Event App",
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Find the best event near you with just one of the best app.",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+                MaterialButton(
+                  onPressed: () {},
+                  child: Text("Get Started"),
+                )
+              ],
+            ),
           ),
         ],
       ),
