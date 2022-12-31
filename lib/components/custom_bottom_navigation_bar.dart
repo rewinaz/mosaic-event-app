2e6ac520dc85922b95674dc5d1b6d6cd791f1baf
rewina zerou <rewinazerou6@gmail.com>
2022-12-31 23:47:22 +0300

    some fix

--- lib/components/custom_bottom_navigation_bar.dart
+++ lib/components/custom_bottom_navigation_bar.dart
@@ -2,15 +2,18 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class CustomBottomAppBar extends StatelessWidget {
-  const CustomBottomAppBar({super.key});
+  CustomBottomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
     Color defaultColor = Colors.white;
     return CurvedNavigationBar(
+        key: _bottomNavigationKey,
         backgroundColor: const Color.fromRGBO(249, 249, 255, 1),
         color: const Color.fromRGBO(46, 137, 232, 1),
-        onTap: (value) => {print(value)},
+        index: 1,
+        onTap: (value) {},
         items: [
           Icon(Icons.explore, color: defaultColor),
           Icon(Icons.home, color: defaultColor),
