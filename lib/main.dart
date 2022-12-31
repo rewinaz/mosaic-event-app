de791058691c39ce4b94d4e2db305d48128b1917
rewina zerou <rewinazerou6@gmail.com>
2022-12-31 23:42:33 +0300

    Firebase, firestore, storage packages and initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,44 @@
-import 'package:event_app/components/custom_bottom_navigation_bar.dart';
-import 'package:event_app/screens/add_new_event_screen.dart';
+import 'package:event_app/firebase_options.dart';
+import 'package:event_app/helpers/utils.dart';
+import 'package:event_app/screens/auth_screen.dart';
 import 'package:event_app/screens/home_screen.dart';
-import 'package:event_app/screens/onboarding_screen.dart';
 import 'package:event_app/screens/signin_screen.dart';
 import 'package:event_app/screens/signup_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scaffoldMessengerKey: Utils.messengerKey,
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Mosaic',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return AuthScreen();
+          }
+        },
       ),
-      home: AddNewEventScreen(),
     );
   }
 }
